767b2541d809f1d84dbbebe2cb6098a40ed4bad6
이상준 <sangjunerhee@isangjun-ui-MacBookPro.local>
2020-12-22 01:08:54 +0900

    Third

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,24 +7,39 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Visualized Sort Algorithm</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div>
+        <h1 className='title'>Visualized Sort Algorithm</h1>
 
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p>
+        <p className='description'>
           Get started by editing{' '}
           <code>pages/index.js</code>
         </p>
         
         <InsertionSort/>
-        
-      </main>
-
+          
+      </div>
+      
+      <style jsx> {`
+        .hero {
+          width: 100%;
+          color: #333;
+        }
+        .title {
+          margin: 0;
+          width: 100%
+          padding-top: 80px;
+          line-height: 1.15;
+          font-size: 48px;
+        }
+        .title,
+        .description {
+          text-align: center;
+        }
+      `}  
+      </style>
       
     </div>
   )
