fa5c77dffa46a1396f1dd2276da4b4ae4b556066
이상준 <sangjunerhee@isangjun-ui-MacBookPro.local>
2020-12-22 00:36:51 +0900

    Apply to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,24 @@
 import React from "react"
 import Head from 'next/head'
 import InsertionSort from '../components/insertionSort'
-import styles from '../styles/Home.module.css'
+//import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
+        <p>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code>pages/index.js</code>
         </p>
         
         <InsertionSort/>
